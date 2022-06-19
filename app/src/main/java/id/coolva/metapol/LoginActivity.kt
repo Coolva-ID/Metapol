e6576a17ce52a949541cb9e87bbd68e35a870d26
mralifakbar <alif.akbarkartadinata2049@students.unila.ac.id>
2022-06-19 20:29:36 +0700

    add login ui

---
+++ app/src/main/java/id/coolva/metapol/LoginActivity.kt
@@ -0,0 +1,11 @@
+package id.coolva.metapol
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
