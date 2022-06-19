1110cb7418bedaa1a14b8a571170982c7e689f8e
Muhammad Wafa <muhammadwafa105@gmail.com>
2022-06-19 22:29:23 +0700

    add bottom navigation

--- app/src/main/java/id/coolva/metapol/LoginActivity.kt
+++ app/src/main/java/id/coolva/metapol/LoginActivity.kt
@@ -1,11 +1,22 @@
 package id.coolva.metapol
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import id.coolva.metapol.databinding.ActivityLoginBinding
+import id.coolva.metapol.ui.main.MainActivity
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnLogin.setOnClickListener {
+            val moveToMain = Intent(this, MainActivity::class.java)
+            startActivity(moveToMain)
+        }
     }
 }
---
+++ app/src/main/java/id/coolva/metapol/ui/main/MainActivity.kt
@@ -0,0 +1,30 @@
+package id.coolva.metapol.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import id.coolva.metapol.R
+import id.coolva.metapol.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_history, R.id.navigation_profile
+            )
+        )
+        navView.setupWithNavController(navController)
+    }
+}
