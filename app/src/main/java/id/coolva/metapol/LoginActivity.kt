4546821d4d7136d2eb30e5764f709d852b89fe93
mralifakbar <alif.akbarkartadinata2049@students.unila.ac.id>
2022-06-20 08:03:41 +0700

    add register ui

--- app/src/main/java/id/coolva/metapol/LoginActivity.kt
+++ app/src/main/java/id/coolva/metapol/LoginActivity.kt
@@ -1,11 +1,64 @@
 package id.coolva.metapol
 
+import android.content.Context
+import android.content.DialogInterface
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AlertDialog
+import id.coolva.metapol.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.btnLogin.setOnClickListener(object: View.OnClickListener {
+            override fun onClick(p0: View?) {
+                val moveToMain = Intent(this@LoginActivity, MainActivity::class.java)
+                startActivity(moveToMain)
+                finish()
+            }
+        })
+
+        binding.btnRegisterInLogin.setOnClickListener(object: View.OnClickListener {
+            override fun onClick(p0: View?) {
+                val moveToLogin = Intent(this@LoginActivity, RegisterActivity::class.java)
+                startActivity(moveToLogin)
+                finish()
+            }
+        })
+    }
+
+    var doubleBackToExitOnce:Boolean = false
+
+    override fun onBackPressed() {
+        if(doubleBackToExitOnce){
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("Keluar Aplikasi")
+            builder.setMessage("Apakah yakin ingin keluar?")
+                .setCancelable(false)
+                .setPositiveButton("Yakin", DialogInterface.OnClickListener { dialogInterface, i ->
+                    finish()
+                })
+                .setNegativeButton("Tidak", DialogInterface.OnClickListener { dialogInterface, i ->
+                    dialogInterface.cancel()
+                })
+
+            val alertDialog:AlertDialog = builder.create()
+            alertDialog.show()
+        }
+
+        this.doubleBackToExitOnce = true
+
+        Handler().postDelayed({
+            kotlin.run { doubleBackToExitOnce = false }
+        }, 2000)
     }
 }
--- app/src/main/java/id/coolva/metapol/RegisterActivity.kt
+++ app/src/main/java/id/coolva/metapol/RegisterActivity.kt
@@ -1,15 +1,40 @@
 package id.coolva.metapol
 
 import android.content.DialogInterface
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.view.View
 import androidx.appcompat.app.AlertDialog
+import id.coolva.metapol.databinding.ActivityLoginBinding
+import id.coolva.metapol.databinding.ActivityRegisterBinding
 
-class MainActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_register)
+
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.btnRegister.setOnClickListener(object: View.OnClickListener {
+            override fun onClick(p0: View?) {
+                val moveToMainActivity = Intent(this@RegisterActivity, MainActivity::class.java)
+                startActivity(moveToMainActivity)
+                finish()
+            }
+        })
+
+        binding.btnLoginInRegister.setOnClickListener(object: View.OnClickListener {
+            override fun onClick(p0: View?) {
+                val moveToLoginActivity = Intent(this@RegisterActivity, LoginActivity::class.java)
+                startActivity(moveToLoginActivity)
+                finish()
+            }
+        })
     }
 
     var doubleBackToExitOnce:Boolean = false
