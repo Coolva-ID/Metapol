8c527d86cc6a72afe29c3450c0803701ed190484
Muhammad Wafa <muhammadwafa105@gmail.com>
2022-06-20 18:53:05 +0700

    add upload file pdf

--- app/src/main/java/id/coolva/metapol/ui/main/form/DaftarSIM.kt
+++ app/src/main/java/id/coolva/metapol/ui/main/form/DaftarSIM.kt
@@ -1,33 +1,104 @@
 package id.coolva.metapol.ui.main.form
 
-import android.graphics.Color
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.drawable.ColorDrawable
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
-import android.view.View
-import android.widget.AdapterView
+import android.util.Log
 import android.widget.ArrayAdapter
-import androidx.navigation.ui.AppBarConfiguration
-import com.google.android.material.appbar.AppBarLayout
+import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import id.coolva.metapol.R
 import id.coolva.metapol.databinding.ActivityDaftarSimBinding
 
+
 class DaftarSIM : AppCompatActivity() {
-    private lateinit var binding: ActivityDaftarSimBinding
+
+    private val binding by lazy {
+        ActivityDaftarSimBinding.inflate(layoutInflater)
+    }
+    private var fileUri = ""
+    private var filepath = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityDaftarSimBinding.inflate(layoutInflater)
+
         setContentView(binding.root)
         val actionBar = supportActionBar
         actionBar!!.title = "Pendaftaran Ujian SIM"
         actionBar!!.setBackgroundDrawable(ColorDrawable(0xFFFFFFFF.toInt()))
 
-        val golSim:Array<String> = resources.getStringArray(R.array.golongan_sim)
-        val arrayAdapterGolSim:ArrayAdapter<String> = ArrayAdapter(this, android.R.layout.simple_list_item_1, golSim)
+        val golSim: Array<String> = resources.getStringArray(R.array.golongan_sim)
+        val arrayAdapterGolSim: ArrayAdapter<String> =
+            ArrayAdapter(this, android.R.layout.simple_list_item_1, golSim)
         binding.actGolSim.setAdapter(arrayAdapterGolSim)
 
-        val sertSim:Array<String> = resources.getStringArray(R.array.sert_mengemudi)
-        val arrayAdapterSertSim:ArrayAdapter<String> = ArrayAdapter(this, android.R.layout.simple_list_item_1, sertSim)
+        val sertSim: Array<String> = resources.getStringArray(R.array.sert_mengemudi)
+        val arrayAdapterSertSim: ArrayAdapter<String> =
+            ArrayAdapter(this, android.R.layout.simple_list_item_1, sertSim)
         binding.actSertMengemudi.setAdapter(arrayAdapterSertSim)
+
+        binding.btnUploadSignature.setOnClickListener {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                // when permission is not granted
+                // request permission
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE) ,
+                    1
+                )
+            } else {
+                // when permission is granted
+                selectPDF()
+            }
+        }
+    }
+
+
+    var sActivityResultLauncher = registerForActivityResult(
+        StartActivityForResult(),
+        ActivityResultCallback<ActivityResult> { result ->
+            if (result.resultCode == RESULT_OK) {
+                val data: Intent? = result.data
+                if (data != null){
+                    // get PDF uri
+                    val uri: Uri? = data.data
+                    Log.e("DAFTAR SIM URI: ", uri.toString())
+
+                    // get PDF path
+                    val path = uri?.path
+                    Log.e("DAFTAR SIM PATH: ", path.toString())
+                }
+            }
+        }
+    )
+
+    private fun selectPDF() {
+            val data = Intent(Intent.ACTION_OPEN_DOCUMENT)
+            data.type = "application/pdf"
+            val result = Intent.createChooser(data, "Choose a file")
+            sActivityResultLauncher.launch(data)
+            Log.e("DAFTAR SIM RESULT: ", result.toString())
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        // check condition
+        if (requestCode == 1 && grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            selectPDF()
+        } else {
+            Toast.makeText(this, "Permission Denied.", Toast.LENGTH_SHORT).show()
+        }
     }
 }
