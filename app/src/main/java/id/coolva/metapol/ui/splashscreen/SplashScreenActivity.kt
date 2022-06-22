a01239df901853d1d136059767ea4c8c873fda4a
Muhammad Wafa <muhammadwafa105@gmail.com>
2022-06-22 22:32:27 +0700

    change launcher icon and splashscreen animation

--- app/src/main/java/id/coolva/metapol/ui/splashscreen/SplashScreenActivity.kt
+++ app/src/main/java/id/coolva/metapol/ui/splashscreen/SplashScreenActivity.kt
@@ -5,17 +5,36 @@ import android.os.Bundle
 import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
 import id.coolva.metapol.R
+import id.coolva.metapol.databinding.ActivitySplashscreenBinding
 import id.coolva.metapol.ui.auth.LoginActivity
 
 class SplashScreenActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashscreenBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splashscreen)
-        val handler = Handler()
-        handler.postDelayed({
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
-            finish()
-        }, 2500)
+        binding = ActivitySplashscreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+//        val handler = Handler()
+//        handler.postDelayed({
+//            val intent = Intent(this, LoginActivity::class.java)
+//            startActivity(intent)
+//            finish()
+//        }, 1000)
+
+        binding.apply {
+            ivMetapolLogo.alpha = 0f
+            tvMetapol.alpha = 0f
+
+            tvMetapol.animate().setDuration(1000).alpha(1f)
+            ivMetapolLogo.animate().setDuration(1000).alpha(1f).withEndAction {
+                val moveToMainActivity = Intent(this@SplashScreenActivity, LoginActivity::class.java)
+                startActivity(moveToMainActivity)
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+                finish()
+            }
+        }
     }
 }
