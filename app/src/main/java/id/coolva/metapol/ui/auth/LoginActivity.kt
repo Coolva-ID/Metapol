b4e65ea487aec709683c63a2a40190293953e559
Muhammad Wafa <muhammadwafa105@gmail.com>
2022-06-22 18:06:58 +0700

    add login authentication

--- app/src/main/java/id/coolva/metapol/ui/auth/LoginActivity.kt
+++ app/src/main/java/id/coolva/metapol/ui/auth/LoginActivity.kt
@@ -5,40 +5,137 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
-import android.view.View
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.app.AlertDialog
+import dagger.hilt.android.AndroidEntryPoint
+import id.coolva.metapol.core.domain.model.User
 import id.coolva.metapol.databinding.ActivityLoginBinding
 import id.coolva.metapol.ui.main.MainActivity
+import id.coolva.metapol.ui.main.profile.UserViewModel
+import id.coolva.metapol.utils.DummyData
+import id.coolva.metapol.utils.Preferences
 
+@AndroidEntryPoint
 class LoginActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityLoginBinding
+    private val viewModel: UserViewModel by viewModels()
+    private val userList = ArrayList<User>()
+    private lateinit var preferences: Preferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
-        binding.btnLogin.setOnClickListener(object: View.OnClickListener {
-            override fun onClick(p0: View?) {
+        // setup preference
+        preferences = Preferences(this)
+        
+        // check if user already login, then intent to MainActivity
+        if (preferences.getValues("loginStatus").equals("1")){
+            finishAffinity()
+
+            val moveToMain = Intent(this@LoginActivity, MainActivity::class.java)
+            startActivity(moveToMain)
+        }
+        
+        observeUserList()
+
+        binding.btnLogin.setOnClickListener {
+            login()
+        }
+
+        binding.btnRegisterInLogin.setOnClickListener {
+            val moveToLogin = Intent(this@LoginActivity, RegisterActivity::class.java)
+            startActivity(moveToLogin)
+            finish()
+        }
+    }
+
+    private fun observeUserList() {
+        viewModel.getUserList().observe(this) { list ->
+            if (list != null) {
+                if (list.isEmpty()) {
+                    val userList = DummyData.provideUserList()
+                    viewModel.insertUser(userList[0])
+                    viewModel.insertUser(userList[1])
+                } else if (list.isNotEmpty()) {
+                    this.userList.addAll(list)
+                    Log.e("LoginActivity: ", list.toString())
+                    Log.e("LoginActivity: ", this.userList.toString())
+                }
+            }
+        }
+    }
+
+    private fun login() {
+        var inputValid = true
+
+        binding.apply {
+            // get user input
+            val userEmail = edtEmailLogin.text.toString()
+            val userPassword = edtPwLogin.text.toString()
+
+            if (userEmail.isEmpty()) {
+                edtEmailLogin.error = "Anda belum memasukkan email"
+                inputValid = false
+            }
+
+            if (userPassword.isEmpty()) {
+                edtPwLogin.error = "Anda belum memasukkan password"
+                inputValid = false
+            }
+
+            if (inputValid) {
+                loginVerification(userEmail, userPassword)
+            }
+        }
+    }
+
+    private fun loginVerification(inputEmail: String, inputPassword: String) {
+
+        val user: User? = getUser(inputEmail)
+        Log.e("LoginActivity: ", user.toString())
+        Log.e("LoginActivity: ", user?.password.toString())
+        Log.e("LoginActivity: ", inputPassword)
+        Log.e("LoginActivity: ", (user?.password == inputPassword).toString())
+
+        if (user != null){
+            if (user.password == inputPassword){
+                // if input valid, save data and intent to Main Activity
+                preferences.setValues("name", user.name)
+                preferences.setValues("email", user.email)
+                preferences.setValues("phoneNumber", user.phoneNumber)
+                preferences.setValues("loginStatus", "1") // if equal to 1, that means user is logged in
+
                 val moveToMain = Intent(this@LoginActivity, MainActivity::class.java)
                 startActivity(moveToMain)
                 finish()
+            } else {
+                binding.edtPwLogin.error = "Password salah."
             }
-        })
-
-        binding.btnRegisterInLogin.setOnClickListener(object: View.OnClickListener {
-            override fun onClick(p0: View?) {
-                val moveToLogin = Intent(this@LoginActivity, RegisterActivity::class.java)
-                startActivity(moveToLogin)
-                finish()
-            }
-        })
+        } else {
+            binding.edtEmailLogin.error = "Email tidak terdaftar."
+        }
     }
 
-    var doubleBackToExitOnce:Boolean = false
+    private fun getUser(inputEmail: String): User?{
+        for (user in userList) {
+            if (user.email == inputEmail) {
+                return user
+            }
+        }
+        return null
+    }
+
+
+    private var doubleBackToExitOnce: Boolean = false
 
     override fun onBackPressed() {
-        if(doubleBackToExitOnce){
+        if (doubleBackToExitOnce) {
             val builder = AlertDialog.Builder(this)
             builder.setTitle("Keluar Aplikasi")
             builder.setMessage("Apakah yakin ingin keluar?")
@@ -50,7 +147,7 @@ class LoginActivity : AppCompatActivity() {
                     dialogInterface.cancel()
                 })
 
-            val alertDialog:AlertDialog = builder.create()
+            val alertDialog: AlertDialog = builder.create()
             alertDialog.show()
         }
 
---
+++ app/src/main/java/id/coolva/metapol/utils/DummyData.kt
@@ -0,0 +1,32 @@
+package id.coolva.metapol.utils
+
+import id.coolva.metapol.core.domain.model.User
+
+object DummyData {
+
+    fun provideUserList(): List<User> {
+        val userList = ArrayList<User>()
+        userList.add(
+            User(
+                name = "Jamal Akhyer",
+                email = "alif.akbarkartadinata2049@students.unila.ac.id",
+                password = "uvuvwevwevwe",
+                phoneNumber = "",
+                profilePhoto = "".toByteArray()
+            )
+        )
+
+        userList.add(
+            User(
+                name = "Muhammad Wafa",
+                email = "muhammadwafa105@gmail.com",
+                password = "passwordmetapol",
+                phoneNumber = "081271306749",
+                profilePhoto = "".toByteArray()
+            )
+        )
+
+
+        return userList
+    }
+}
